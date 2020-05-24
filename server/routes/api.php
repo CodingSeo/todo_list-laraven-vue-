2c04284b15d1bda7d784a02e6583df1b16582ad8
CodingSeo <seojingyo@hotmail.com>
2020-05-24 13:35:44 +0900

    ##Api routing
    API 라우팅을 위해서 host/api를 사용하였다.

--- server/routes/api.php
+++ server/routes/api.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('task','TaskController@index');
+Route::get('task/{id}','TaskController@show');
+Route::post('task','TaskController@store');
+Route::put('task/{id}','TaskController@update');
+Route::delete('task/{id}','TaskController@delete');
